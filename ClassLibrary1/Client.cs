2636c675ab5112284498184d3cea64c653acf49d
liouyuku <liouyuku@163.com>
2015-04-06 15:50:35 +0800

    submit

--- ClassLibrary1/Client.cs
+++ ClassLibrary1/Client.cs
@@ -4,12 +4,10 @@ using BeefDLL.util;
 using System;
 using System.Collections.Generic;
 using System.IO;
-using System.Linq;
 using System.Net;
 using System.Net.Sockets;
 using System.Text;
 using System.Threading;
-using System.Threading.Tasks;
 
 namespace BeefDLL
 {
@@ -199,7 +197,7 @@ namespace BeefDLL
         }
 
 
-
+        //貌似有问题啊啊.
     }
 
 
